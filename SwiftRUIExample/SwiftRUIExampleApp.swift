6317dc568c9be7ffcbcb9d709a4e0916b1f04c10
Swee Kwang <sweekwang94@gmail.com>
2024-07-02 21:45:42 +0800

    Initial Commit

---
+++ SwiftRUIExample/SwiftRUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRUIExampleApp.swift
+//  SwiftRUIExample
+//
+//  Created by Swee Kwang Chua on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
